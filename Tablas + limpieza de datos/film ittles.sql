a91bdf0e5d7445c5137baaa3f17e0f6d9c9c92b5
Juan Boberg Aguirre <tu-email@example.com>
2026-04-29 17:31:20 +0200

    update

--- Tablas + limpieza de datos/film ittles.sql
+++
@@ -1,9 +0,0 @@
- USE sakila;
-SHOW TABLES LIKE 'film';
- SELECT title, length
-FROM film
-WHERE length > (
-    SELECT AVG(length)
-    FROM film
-)
-ORDER BY length DESC;
--- Tablas + limpieza de datos/tabla tracks.sql
+++
@@ -1,7 +0,0 @@
--- Esto borra la tabla si existe, evitando el error 1050
-DROP TABLE IF EXISTS Tracks;
-
--- Ahora puedes crearla con total seguridad
-CREATE TABLE Tracks AS
-SELECT DISTINCT track_id, track_name, album_name, popularity, duration_ms, explicit, track_genre
-FROM dataset;
